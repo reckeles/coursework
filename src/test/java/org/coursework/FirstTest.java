1e9f4f378ae0b79f02aed675f70457fc0afe0753
Yuliia <yuliia@Yuliias-MacBook-Pro.local>
2023-10-02 18:51:06 +0300

    added allure report

--- src/test/java/org/coursework/FirstTest.java
+++ src/test/java/org/coursework/FirstTest.java
@@ -1,6 +1,9 @@
 package org.coursework;
 
+import io.qameta.allure.Step;
 import org.coursework.base.BaseGUITest;
+import org.coursework.pages.Login;
+import org.openqa.selenium.bidi.log.Log;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,11 +14,22 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class FirstTest extends BaseGUITest {
     @Test
-    public void firsttest() throws URISyntaxException {
-        $("input#form-username").sendKeys("admin");
-        $("input#form-password").sendKeys("admin");
-        $x("//button[@type='submit']").click();
+    public void firsttest() {
+        Login loginPage = new Login();
+        loginPage.fillInUsername();
+        loginPage.fillInPassword();
+        loginPage.clickButton();
+        trydjs();
+//        $("input#form-username").sendKeys("admin");
+//        $("input#form-password").sendKeys("admin");
+//        $x("//button[@type='submit']").click();
+
 
         Assert.assertEquals($(".alert").getText(), "There is nothing assigned to you.");
+    }
+
+    @Step
+    public void trydjs(){
+        System.out.println("dsjkf");
     }
 }
