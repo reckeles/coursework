5b32c0752a829d8e4505000bfb7c6a594284dc80
Yuliia <yuliia@Yuliias-MacBook-Pro.local>
2023-10-12 14:06:34 +0300

    added browser options

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-To run all tests ``mvn clean test -Denv=DEV -Dlocale=EN``
+To run all tests ``mvn clean test``
 
-To run one specific test `` mvn clean test -Denv=DEV -Dlocale=EN -Dtest={classname}#{testname}
+To run one specific test `` mvn clean test-Dtest={classname}#{testname}
 ``
 
 To generate report ``mvn allure:report``
+
+Options for test run 
+1. ``-Denv``, default value ``dev``
+2. ``-Dlocale``, default value ``en``
+3. ``-Dbrowser``, default value ``chrome``
+4. ``-Dheadless``, default value ``true``
--- src/main/java/org/coursework/Session.java
+++ src/main/java/org/coursework/Session.java
@@ -4,16 +4,17 @@ import org.coursework.config.EnvConfig;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Session {
     static final private ThreadLocal<Session> _instance = new ThreadLocal<>();
-
-    private Session() {
-        Runtime.getRuntime().addShutdownHook(new Thread(Session.this::close));
-    }
+    private WebDriver _webdriver;
+    private final boolean HEADLESS_FLAG =  setHeadlessFlag();
+    private final String WEB_BROWSER = setWebBrowser();
 
     static public Session get() {
         if (_instance.get() == null)
@@ -21,42 +22,39 @@ public class Session {
         return _instance.get();
     }
 
-    private WebDriver _webdriver;
-
     public WebDriver webdriver() {
         if (this._webdriver == null) {
-            if ("chrome".equalsIgnoreCase(EnvConfig.WEB_BROWSER.value)) {
+            if ("chrome".equalsIgnoreCase(WEB_BROWSER)) {
                 ChromeOptions options = new ChromeOptions();
-//                options.addArguments("wm-window-animations-disabled");
-//                options.addArguments("ash-disable-smooth-screen-rotation");
-//                options.addArguments("disable-smooth-scrolling");
-//                options.addArguments("disable-infobars");
                 options.addArguments("disable-default-apps");
                 options.addArguments("disable-extensions");
-                //options.addArguments("lang=en_US");
-//                options.setAcceptInsecureCerts(true);
-                //options.addArguments("auto-open-devtools-for-tabs");
                 Map<String, Object> preferences = new HashMap<>();
-//                preferences.put("history.saving_disabled", true);
                 preferences.put("browser.show_home_button", false);
                 options.addArguments("start-maximized");
-//                preferences.put("credentials_enable_service", false);
-//                preferences.put("profile.password_manager_enabled", false);
-//                options.setExperimentalOption("prefs", preferences);
-//                options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation", "load-extension"});
-//                if (EnvConfig.WEB_BROWSER_NO_GUI.isTrue()) {
-//                    options.addArguments("--headless");
-//                    options.addArguments("--no-sandbox");
-//                    options.addArguments("start-maximized");
-//                }
+                if (HEADLESS_FLAG) {
+                    options.addArguments("--headless");
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("start-maximized");
+                }
                 this._webdriver = new ChromeDriver(options);
-//                this._webdriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                 this._webdriver.manage().window().maximize();
-//                this._webdriver.manage().window().setSize(new Dimension(5000, 5000));
+            }
+            if ("firefox".equalsIgnoreCase(WEB_BROWSER)) {
+                FirefoxOptions options = new FirefoxOptions();
+                options.addArguments("disable-default-apps");
+                options.addArguments("disable-extensions");
+                Map<String, Object> preferences = new HashMap<>();
+                preferences.put("browser.show_home_button", false);
+                options.addArguments("start-maximized");
+                if (HEADLESS_FLAG) {
+                    options.addArguments("--headless");
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("start-maximized");
+                }
+                this._webdriver = new FirefoxDriver(options);
+                this._webdriver.manage().window().maximize();
             }
         }
-
-
         return this._webdriver;
     }
 
@@ -65,5 +63,31 @@ public class Session {
             this._webdriver.quit();
             this._webdriver = null;
         }
+    }
+
+    private Session() {
+        Runtime.getRuntime().addShutdownHook(new Thread(Session.this::close));
+    }
+
+    //TO DO - refactor if has time
+    private boolean setHeadlessFlag(){
+        String property = System.getProperty("headless");
+        if(property==null){
+            return true;
+        }
+        return Boolean.valueOf(property);
+    }
+
+    private String setWebBrowser(){
+        String property = System.getProperty("browser");
+        String chrome = "chrome";
+        String firefox = "firefox";
+        if(property==null){
+            return chrome;
+        }
+        else if (!property.equalsIgnoreCase(chrome) || !property.equalsIgnoreCase(firefox)){
+            return chrome;
+        }
+        return property;
     }
 }
